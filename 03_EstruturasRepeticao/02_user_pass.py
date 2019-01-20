1ba1e9583187225b665792be24a751772a532f3b
guisotsek <46506025+guisotsek@users.noreply.github.com>
2019-01-20 20:03:45 -0200

    Update 02_user_pass.py

--- 03_EstruturasRepeticao/02_user_pass.py
+++ 03_EstruturasRepeticao/02_user_pass.py
@@ -4,3 +4,15 @@ while (usuario == senha):
     senha = raw_input('Informe a senha: ')
     if (usuario == senha):
         print 'A senha nao pode ser igual ao nome do usuario'
+
+#outa forma de se fazer, quem sabe até melhor ;D
+#while True:
+#    nome = str(input("Nome [usuário]: ")).strip()
+#    senha = str(input("Senha: "))
+#    if nome == senha:
+#        print("{:-^10}".format("ERRO"))
+#        print("Você não pode adicionar como senha o seu nome.")
+#    else:
+#        print("_" * 30)
+#        print(f"Cadastrado. Seja bem vindo {nome}.")
+#        break

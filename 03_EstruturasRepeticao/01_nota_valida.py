706fcff1b900f637ca0dc57a9e102299695989d5
guisotsek <46506025+guisotsek@users.noreply.github.com>
2019-01-20 19:55:50 -0200

    Update 01_nota_valida.py

--- 03_EstruturasRepeticao/01_nota_valida.py
+++ 03_EstruturasRepeticao/01_nota_valida.py
@@ -3,3 +3,12 @@ while (nota < 0) or (nota > 10):
     nota = int(raw_input('Informe uma nota: '))
     if (nota < 0) or (nota > 10):
         print 'Nota Invalida'
+
+# outro jeito de se fazer: (quem sabe até melhor ;D )
+#while True:
+#    nota =  int(input("Informe uma nota [de 0 a 10]: "))
+#    if nota < 0 or nota > 10:
+#        print("Tente novamente.")
+#    else:
+#        print(f"Nota informada: {nota}")
+#        break

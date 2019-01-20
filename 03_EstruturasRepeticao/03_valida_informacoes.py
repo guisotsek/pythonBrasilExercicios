5a507c5bd9e684f8b7c2913a83bad132721186b2
guisotsek <46506025+guisotsek@users.noreply.github.com>
2019-01-20 20:26:18 -0200

    Update 03_valida_informacoes.py

--- 03_EstruturasRepeticao/03_valida_informacoes.py
+++ 03_EstruturasRepeticao/03_valida_informacoes.py
@@ -1,35 +1,33 @@
-# Validacao do nome
-nome = ''
-while (len(nome) <= 3):
-    nome = raw_input('Informe um nome: ')
-    if (len(nome) <= 3):
-        print 'O nome deve ter mais de tres letras!'
-
-# Validacao da idade
-idade = -1
-while (idade < 0) or (idade > 150):
-    idade = int(raw_input('Informe a idade: '))
-    if (idade < 0) or (idade > 150):
-        print 'Idade deve estar entre 0 e 150'
-
-# Validacao do salario
-salario = 0
-while (salario <= 0):
-    salario = int(raw_input('Informe o salario: '))
-    if (salario <= 0):
-        print 'O salario deve ser maior que zero'
-
-# Valida o sexo
-sexo = ''
-while (sexo != 'F') and (sexo != 'M'):
-    sexo = raw_input('Informe o sexo: ').upper()
-    if (sexo != 'F') and (sexo != 'M'):
-        print 'O sexo deve ser infomado como M (masculino) ou F (feminino)'
-
-# Valida o estado civil
-estado_civil = 'A'
-while ('SCVD'.find(estado_civil) < 0):
-    estado_civil = raw_input('Informe o estado civil: ').upper()
-    if ('SCVD'.find(estado_civil) < 0):
-        print 'Estado civil deve ser informado como S (solteiro), C (casado),'\
-            ' V (viuvo) ou D (divorciado)'
+# Validacao do nome, melhorado.
+while True:
+    while True:
+        nome = str(input("Nome: ")).strip()
+        if len(nome) <= 3:
+            print("Nome inválido, ele deve ter mais de 3 letras. Tente novamente.")
+        else:
+            break
+    while True:
+        idade = int(input("Idade: "))
+        if idade < 0 or idade > 150:
+            print("Idade válida apenas entre 0 e 150. Tente novamente.")
+        else:
+            break
+    while True:
+        sexo = str(input("Sexo [M/F]: ")).strip().upper()[0]
+        if sexo not in "FM":
+            print("Sexo inválido, válido apenas se M(masculino) ou F(feminino). Tente novamente")
+        else:
+            break
+    while True:
+        sal = float(input("Salário: R$"))
+        if sal == 0:
+            print("Salário inválido, ele deve ser maior que zero(0). Tente novamente.")
+        else:
+            break
+    while True:
+        est_civil = str(input("Estado civil [S/C/V/D]: ")).strip().upper()[0]
+        if  est_civil not in "SCVD":
+            print("Estado civil inválido. Tente novamente.")
+        else:
+            break
+    break
